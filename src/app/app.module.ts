c78675b290c627895163fb0bdb375c72ae510d8f
Aleksandr <duxard@gmail.com>
2020-02-25 12:24:37 -0500

    add news-feed component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsFeedComponent
   ],
   imports: [
     BrowserModule,
