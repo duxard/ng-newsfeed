d829af28eb414d14de5e3ff00b7f7de73a96c21a
Aleksandr <duxard@gmail.com>
2020-02-25 12:49:02 -0500

    add message form component

---
+++ src/app/add-message-form/add-message-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-message-form',
+  templateUrl: './add-message-form.component.html',
+  styleUrls: ['./add-message-form.component.scss']
+})
+export class AddMessageFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { AddMessageFormComponent } from './add-message-form/add-message-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsFeedComponent
+    NewsFeedComponent,
+    AddMessageFormComponent
   ],
   imports: [
     BrowserModule,
