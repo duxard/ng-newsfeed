ca36401b87a9841cfadb85ff70c45c97b01b6c26
duxard <duxard@gmail.com>
2020-03-03 22:25:20 +0300

    modify markup

--- src/app/shared/validators/nowhitespaces.validator.ts
+++ src/app/shared/validators/nowhitespaces.validator.ts
@@ -2,5 +2,5 @@ import { AbstractControl } from '@angular/forms';
 
 export function NoWhitespaces(control: AbstractControl) {
   const isInputEmpty = control.value.trim().length === 0;
-  return isInputEmpty ? { 'emptystring': true } : null;
+  return isInputEmpty ? { emptyString: true } : null;
 }
