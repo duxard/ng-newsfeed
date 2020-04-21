f88d16d21bbcb2daa0c2797762f70b497bbbd9bc
duxard <duxard@gmail.com>
2020-04-21 10:58:24 +0300

    create components subfolder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommentsFeedComponent } from './comments-feed/comments-feed.component';
-import { AddCommentFormComponent } from './add-comment-form/add-comment-form.component';
+import { CommentsFeedComponent } from './components/comments-feed/comments-feed.component';
+import { AddCommentFormComponent } from './components/add-comment-form/add-comment-form.component';
 import { HttpClientModule } from '@angular/common/http';
-import { UploadSpinnerComponent } from './upload-spinner/upload-spinner.component';
+import { UploadSpinnerComponent } from './components/upload-spinner/upload-spinner.component';
 
 @NgModule({
   declarations: [
